2d2deff451afa697a360e22327b9af409bd15297
Jacqueline-Salinas <jacqueline.salinas@netdatabio.cl>
2023-12-06 21:54:10 -0300

    se crean productos de prueba

--- project/producto/forms.py
+++ project/producto/forms.py
@@ -1,8 +1,17 @@
 from django import forms
-from .models import *
+from .models import Proveedor, Producto, Stock
+
+class ProveedorForm(forms.ModelForm):
+    class Meta:
+        model = Proveedor
+        fields = '__all__'  # Puedes especificar los campos si no quieres incluirlos todos
 
 class ProductoForm(forms.ModelForm):
     class Meta:
         model = Producto
-        fields = '__all__'
-        
+        fields = '__all__'  # Puedes especificar los campos si no quieres incluirlos todos
+
+class StockForm(forms.ModelForm):
+    class Meta:
+        model = Stock
+        fields = '__all__'  # Puedes especificar los campos si no quieres incluirlos todos
--- project/producto/views.py
+++ project/producto/views.py
@@ -4,8 +4,19 @@ from .forms import *
 def home(request):
     return render(request, 'producto/index.html')
 
+from django.shortcuts import render, redirect
+from .forms import ProductoForm
+
 def crear_producto(request):
-    if request.method == 'GET':
-        context = {"form": ProductoForm()}
-        return render(request, 'producto/crear_producto.html', context)
+    if request.method == 'POST':
+        form = ProductoForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('producto:index')
+    else:
+        form = ProductoForm()
+    
+    context = {"form": form}
+    return render(request, 'producto/crear_producto.html', context)
+
         
