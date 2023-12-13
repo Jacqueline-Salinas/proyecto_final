c9761b473097cc164b30462f3bf41ebd82e71e31
Jacqueline-Salinas <jacqueline.salinas@netdatabio.cl>
2023-12-13 07:24:09 -0300

    CRUD de productos

--- project/producto/urls.py
+++ project/producto/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-
 from . import views
 
 app_name = "producto"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path('producto/crear_producto/', views.crear_producto, name="crear_producto"),
+    path('producto/crear_producto/', views.crear_o_actualizar_producto, name="crear_producto"),
     path('producto/crear_proveedor/', views.crear_proveedor, name="crear_proveedor"),
     path('producto/crear_stock/', views.crear_stock, name="crear_stock"),
+    path('producto/actualizar/<int:pk>/', views.crear_o_actualizar_producto, name='actualizar_producto'),
+    path('producto/borrar/<int:pk>/', views.borrar_producto, name='borrar_producto'),
 ]
--- project/producto/views.py
+++ project/producto/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+from django.views.decorators.http import require_POST
 from .forms import *
 from .models import Producto
 
@@ -9,17 +11,20 @@ def lista_productos(request):
     productos = Producto.objects.all()
     return render(request, 'producto/index.html', {'productos': productos})
 
-def crear_producto(request):
+def crear_o_actualizar_producto(request, pk=None):
+    producto = get_object_or_404(Producto, pk=pk) if pk else None
+    
     if request.method == 'POST':
-        form = ProductoForm(request.POST, request.FILES)
+        form = ProductoForm(request.POST, request.FILES, instance=producto)
         if form.is_valid():
             form.save()
-            return redirect('producto:index')
+            return redirect('producto:index')  # Reemplaza 'producto:index' con la URL adecuada
     else:
-        form = ProductoForm()
+        form = ProductoForm(instance=producto)
     
     context = {"form": form}
     return render(request, 'producto/crear_producto.html', context)
+
 
 def crear_proveedor(request):
     if request.method == 'POST':
@@ -65,5 +70,11 @@ def index(request):
             productos = productos.filter(nombre__icontains=nombre)
 
     return render(request, 'producto/index.html', {'productos': productos, 'form': form})
+
+@require_POST
+def borrar_producto(request, pk):
+    producto = Producto.objects.get(pk=pk)
+    producto.delete()
+    return redirect('lista_productos')
 
         
