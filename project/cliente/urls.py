713028634742256c95735b991ae009093ac1045f
Jacqueline-Salinas <jacqueline.salinas@netdatabio.cl>
2023-12-24 22:41:31 -0300

    se agrega iniciar sesion y login funcional

--- project/cliente/urls.py
+++ project/cliente/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path, include
+from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -7,5 +8,6 @@ app_name = "cliente"
 urlpatterns = [
     path('registro/', views.registro, name='registro'),
     path('login/', views.iniciar_sesion, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),  # URL y vista de cierre de sesión
     # Otros paths que puedas necesitar en esta app
 ]
--- project/cliente/views.py
+++ project/cliente/views.py
@@ -1,26 +1,30 @@
 from django.contrib.auth import login, authenticate
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from .forms import ClienteRegistroForm
 from .models import PerfilCliente
-
-from .forms import ClienteRegistroForm
 
 def registro(request):
     if request.method == 'POST':
         form = ClienteRegistroForm(request.POST)
         if form.is_valid():
             user = form.save()  # Guardar el usuario
-            # Crear el perfil del cliente asociado al usuario registrado
-            nombre = form.cleaned_data.get('nombre')
-            apellido = form.cleaned_data.get('apellido')
-            nacionalidad = form.cleaned_data.get('nacionalidad')
-            correo = form.cleaned_data.get('correo')
-            PerfilCliente.objects.create(user=user, nombre=nombre, apellido=apellido, nacionalidad=nacionalidad, correo=correo)
+            perfil_cliente = get_object_or_404(PerfilCliente, user=user)
+            if not perfil_cliente:  # Si no existe, crea uno nuevo
+                perfil_cliente = PerfilCliente.objects.create(
+                    user=user,
+                    nombre=form.cleaned_data.get('nombre'),
+                    apellido=form.cleaned_data.get('apellido'),
+                    nacionalidad=form.cleaned_data.get('nacionalidad'),
+                    correo=form.cleaned_data.get('correo')
+                )
             # Autenticar al usuario recién registrado si es necesario
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=user.username, password=raw_password)
             login(request, user)
-            return redirect('core:index')
+            if request.user.is_authenticated:  # Verificar si el usuario está autenticado
+                return redirect('core:index')  # Redirigir al home principal si está autenticado
     else:
         form = ClienteRegistroForm()
     return render(request, 'cliente/registro.html', {'form': form})
@@ -34,7 +38,11 @@ def iniciar_sesion(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('nombre_de_la_vista')  # Redirigir a donde desees después del inicio de sesión
+            if not user.is_superuser:  # Si el usuario no es un superusuario
+                return redirect('core:index')  # Redirige al home principal
+            else:
+                return redirect('dashboard_admin')  # Redirige al dashboard de admin si es el superusuario
+        # Si el usuario es None o la autenticación falla, puede ser útil agregar un mensaje de error
         else:
             # Manejar la lógica para un inicio de sesión fallido
             pass
